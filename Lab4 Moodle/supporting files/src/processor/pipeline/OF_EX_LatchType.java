7a0d33ea2997f85c2e3f09e0266a93c1254cd6b0
RMan2323 <raghuveer23verma@gmail.com>
2025-03-08 20:18:11 +0530

    Got latest from moodle

---
+++ Lab4 Moodle/supporting files/src/processor/pipeline/OF_EX_LatchType.java
@@ -0,0 +1,40 @@
+package processor.pipeline;
+
+public class OF_EX_LatchType {
+	
+	boolean EX_enable;
+	int imm1, imm2, branchTgt, rd;
+	String operation;
+	
+	public OF_EX_LatchType()
+	{
+		EX_enable = false;
+	}
+
+	public boolean isEX_enable() {
+		return EX_enable;
+	}
+
+	public void setEX_enable(boolean eX_enable) {
+		EX_enable = eX_enable;
+	}
+
+	public void setImm(int op1, int op2){
+		imm1 = op1;  //op1
+		imm2 = op2;  //op2
+	}
+
+	public void setBt(int bt){
+		branchTgt = bt;
+
+	}
+
+	public void setOperation(String op){
+		operation = op;
+	}
+
+	public void setDest(int dest){
+		rd = dest;
+	}
+
+}
---
+++ Lab4/src/processor/pipeline/RegisterWrite.java
@@ -0,0 +1,38 @@
+package processor.pipeline;
+
+import generic.Simulator;
+import processor.Processor;
+
+@SuppressWarnings("unused")
+public class RegisterWrite {
+	Processor containingProcessor;
+	MA_RW_LatchType MA_RW_Latch;
+	IF_EnableLatchType IF_EnableLatch;
+
+	public RegisterWrite(Processor containingProcessor, MA_RW_LatchType mA_RW_Latch,
+			IF_EnableLatchType iF_EnableLatch) {
+		this.containingProcessor = containingProcessor;
+		this.MA_RW_Latch = mA_RW_Latch;
+		this.IF_EnableLatch = iF_EnableLatch;
+	}
+
+	public void performRW() {
+		if (MA_RW_Latch.isRW_enable()) {
+//			MA_RW_Latch.setRW_enable(false);
+			System.out.println("Performing RW!!!!!!");
+			System.out.println("RW "+MA_RW_Latch.endProg);
+
+			if (MA_RW_Latch.endProg) {
+				Simulator.setSimulationComplete(true);
+				System.out.println("----\nENDING");
+			} else {
+				containingProcessor.getRegisterFile().setValue(MA_RW_Latch.rd, MA_RW_Latch.res);
+				System.out.println("----\nPUTTING "+MA_RW_Latch.res+" in register "+MA_RW_Latch.rd);
+				containingProcessor.getRegisterFile().setValue(31, MA_RW_Latch.rem);
+				System.out.println("----\nPUTTING "+MA_RW_Latch.rem+" in register 31");
+			}
+			IF_EnableLatch.setIF_enable(true);
+	}
+
+}
+}
